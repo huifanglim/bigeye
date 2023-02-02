1c5f169ad68cbc55a9af4e6b78c79f3f61482aa6
LimHF <limhuifang123@gmail.com>
2023-02-02 15:50:26 +0800

    add more test code

--- test2.py
+++ test2.py
@@ -1,1 +1,10 @@
-import PIL
+import pytesseract
+from PIL import Image
+
+# Add tesseract to PATH
+pytesseract.pytesseract.tesseract_cmd = r'C:\\Users\\user\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe'
+
+ready_file = "C:/Users/user/Desktop/bigeye/temp/testimg.jpg"
+img = Image.open(ready_file)
+ocr_result = pytesseract.image_to_string(img)
+print(ocr_result)
